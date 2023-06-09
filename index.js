755b4ecb5c02156cb2001a093a3315bbc56bb627
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-06-09 16:46:09 +0300

    modify fetch function

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const apiUrl = "https://api.adviceslip.com/advice"
 const dice = document.querySelector(".circle")
+let previousAdvice = [];
 
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    // Handle the response data here
-    document.querySelector("#advice-id").innerHTML = data.slip.id
-    document.querySelector("#advice-txt").innerHTML = data.slip.advice
-    console.log(data.slip.id);
-  })
-  .catch(error => {
-    // Handle any errors that occur during the request
-    console.log(error);
-  });
 
-  dice.addEventListener('click', ()=>{
-    fetch(apiUrl)
-  })
+function fetchRandomQuote(){
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response data here
+      const adviceId = data.slip.id;
+      const advice = data.slip.advice;
+
+      if (!previousAdvice.includes(adviceId, advice)){
+        previousAdvice.push(adviceId, advice)
+        document.querySelector("#advice-id").innerHTML = adviceId;
+        document.querySelector("#advice-txt").innerHTML = advice;
+      } else {
+        fetchRandomQuote();
+      }
+
+/*       console.log(data.slip.id); */
+    })
+    .catch(error => {
+      // Handle any errors that occur during the request
+      console.log(error);
+    });
+}
+
+dice.addEventListener("click", ()=> {fetchRandomQuote()});
+
+fetchRandomQuote();
